e22ffa772a02413ff3e058c1d655eae230b04b95
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2024-03-16 12:27:26 +0200

    upd

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,3 +1,30 @@
-export const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import { ForbesListItem } from 'components/ForbesListItem/ForbesListItem';
+import style from './ForbesList.module.css';
+export const ForbesList = ({ forbes }) => {
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {forbes.map(item => {
+          return (
+            <li className={style.item} key={item.id}>
+              {' '}
+              <ForbesListItem
+                name={item.name}
+                capital={item.capital}
+                avatar={item.avatar}
+                isIncrease={item.isIncrease}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
